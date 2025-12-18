0a5ba7d11b00ed74ed6b0312da84ea63a908e70d
haer00reh <haytamerrehyby@gmail.com>
2025-12-18 14:09:48 +0100

    UPDATE

--- push_swap.c
+++ push_swap.c
@@ -6,33 +6,17 @@
 /*   By: haer-reh <haer-reh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 15:45:52 by haer-reh          #+#    #+#             */
-/*   Updated: 2025/12/18 13:57:00 by haer-reh         ###   ########.fr       */
+/*   Updated: 2025/12/18 14:09:27 by haer-reh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	parse_input(int ac, char **av)
-{
-
-
-
-
-	
-}
-
-
-int main(int ac, char **av)
+void	parse_input(int ac, char **av, t_list **list)
 {
 	char	**s;
 	int 	(i), (j);
-	t_list	*list;
-	t_stack	(a), (b);
-
-	if (ac < 2)
-		return (-1);
-	if (ac >= 2)
-	{
+	
 	i = 1;
 	list = NULL;
 		while (i < ac)
@@ -46,6 +30,20 @@ int main(int ac, char **av)
 		}
 		i++;
 		}
+	
+}
+
+
+int main(int ac, char **av)
+{
+	t_list	*list;
+	t_stack	(a), (b);
+
+	if (ac < 2)
+		return (-1);
+	if (ac >= 2)
+	{
+	parse_input(ac, av, &list);
 	a.top = list;
 	a.size = ft_lstsize(list);
 	b.top = NULL;
