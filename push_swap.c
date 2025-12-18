5af8c14cdd195785af5a31233604f15fc1cc3948
haer00reh <haytamerrehyby@gmail.com>
2025-12-18 14:04:55 +0100

    Genesis

---
+++ push_swap.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: haer-reh <haer-reh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 15:45:52 by haer-reh          #+#    #+#             */
+/*   Updated: 2025/12/18 13:57:00 by haer-reh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	parse_input(int ac, char **av)
+{
+
+
+
+
+	
+}
+
+
+int main(int ac, char **av)
+{
+	char	**s;
+	int 	(i), (j);
+	t_list	*list;
+	t_stack	(a), (b);
+
+	if (ac < 2)
+		return (-1);
+	if (ac >= 2)
+	{
+	i = 1;
+	list = NULL;
+		while (i < ac)
+		{
+		s = ft_split(av[i], ' ');
+		j = 0;
+		while (s[j])
+		{
+			ft_lstadd_back(&list, ft_lstnew(ft_atoi(s[j])));
+			j++;
+		}
+		i++;
+		}
+	a.top = list;
+	a.size = ft_lstsize(list);
+	b.top = NULL;
+	b.size = 0;
+	}
+	return (0);
+}
---
+++ push_swap.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: haer-reh <haer-reh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 15:46:13 by haer-reh          #+#    #+#             */
+/*   Updated: 2025/12/17 19:37:31 by haer-reh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_list
+{
+	int				content;
+	int				index;
+	int				in_lis;
+	struct s_list	*next;
+}					t_list;
+
+typedef struct s_stack
+{
+	t_list	*top;
+	int		size;
+}	t_stack;
+
+char	**ft_split(char *str, char sep);
+void	free_split(char **split);
+int		count_words(char *str, char sep);
+long	ft_atoi(char *str);
+char	*copy_word(char *str, int len);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstnew(int content);
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	del(void *content);
+
+
+
+#endif
